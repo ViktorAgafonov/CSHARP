56b2cd2d5514af83ced66826169f36b3df4c1977
ViktorAgafonov <v.v.agafonov@inbox.ru>
2022-03-17 23:42:34 +0300

    Eiler chapter 10 on two task

--- Program.cs
+++ Program.cs
@@ -1,28 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyFirstApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            bool a = false;     // 1 bit
-            byte b = (byte)'A'; // 8 bit
-            sbyte s = 0;        // 8 bit
-            char c = 'A';       // 16 bit
-            short s1 = 0;       // 16 bit
-            ushort s2 = 0;      // 16 bit
-            int i = 0;          // 32 bit
-            long l = 0;         // 64 bit
-            decimal d = 0;      // 128 bit
-            
+            Stopwatch time = new Stopwatch();
+            time.Start();
 
-            
-            Console.WriteLine($"Char: {a}, Byte: {b}");
+            decimal limit = 2000000;
+
+            int temp1 = 0;
+            int temp2 = 0;
+            int result = 0;
+
+            decimal limit1 = Math.Ceiling(limit/2);
+            decimal limit2 = limit - limit1;
+
+            Task task1 = new Task(() =>
+            {
+                while (temp1 < limit1)
+                {
+                    temp1++;
+                    for (int i = 2; i <= temp1; i++)
+                    {
+                        if (temp1 % i == 0 && i < temp1)
+                        {
+                            break;
+                        }
+                        else if (temp1 % i == 0 && i == temp1)
+                        {
+                            result += temp1;
+                        }
+                    }
+                }
+            });
+
+            Task task2 = new Task(() =>
+            {
+                while (temp2 < limit2)
+                {
+                    temp2++;
+                    for (int i = 2; i <= temp2; i++)
+                    {
+                        if (temp2 % i == 0 && i < temp2)
+                        {
+                            break;
+                        }
+                        else if (temp2 % i == 0 && i == temp2)
+                        {
+                            result += temp2;
+                        }
+                    }
+                }
+            });
+
+            task1.Start();
+            task2.Start();
+
+            task1.Wait();
+            task2.Wait();
+
+            time.Stop(); 
+            Console.WriteLine(time.Elapsed); 
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
         }
     }
 }
